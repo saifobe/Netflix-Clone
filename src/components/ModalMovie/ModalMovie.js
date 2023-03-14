376d4e88befa705b0a81488c2b165878a06c3b30
saifobe <obeidatsaif93@gmail.com>
2023-03-14 15:40:19 +0000

    lab 18 done

---
+++ src/components/ModalMovie/ModalMovie.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
+
+function ModalMovie(props) {
+  return (
+    <Modal show={props.showFlag} onHide={props.handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{props.movieData.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Image src={props.movieData.image_path} width='100%'></Image>
+                <p>{props.movieData.overview}</p>
+            
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={props.handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+  )
+}
+
+export default ModalMovie
---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import ModalMovie from '../ModalMovie/ModalMovie';
+import './Movie.css'
+
+
+
+function Movie(props) {
+    const [showLines, setShowLines] = useState(false);
+    const [showFlag,setShowFlag] = useState(false);
+    const [clickedMovie, setClickedMovie] = useState({});
+    const handleShow = (props) =>{
+        console.log(props.myMovie);
+        setClickedMovie(props.myMovie);
+        setShowFlag(true);
+    }
+    const handleClose = () =>{
+        setShowFlag(false);
+    }
+  return (
+    <Col>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2`+props.myMovie.poster_path} />
+      <Card.Body>
+        <Card.Title>{props.myMovie.title && <h3>Movie Name: {props.myMovie.title}</h3>}</Card.Title>
+        <h4>ID:{props.myMovie.id}</h4>
+        <Card.Text className={showLines ? '' :'showLess' }>{props.myMovie.overview} </Card.Text>
+        <Button variant="primary" onClick={() => {handleShow(props.myMovie)}}>More Details</Button>
+      </Card.Body>
+    </Card>
+    <ModalMovie showFlag={showFlag} handleClose={handleClose} movieData={clickedMovie}/>
+    </Col>
+    
+  )
+}
+
+export default Movie
